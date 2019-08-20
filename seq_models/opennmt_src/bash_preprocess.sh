c934b26c3ca5f393ef5531928f04edcb26e6ef4f
fdamani <farhand7@gmail.com>
2019-08-20 19:09:40 -0400

    onmt preprocess.

---
+++ seq_models/opennmt_src/bash_preprocess.sh
@@ -0,0 +1,5 @@
+python ../../OpenNMT-py/preprocess.py -train_src /home/fdamani/mol-edit/data/qed/src_train.csv \
+									  -train_tgt /home/fdamain/mol-edit/data/qed/tgt_train.csv \
+									  -valid_src /home/fdamain/mol-edit/data/qed/src_valid.csv \
+									  -valid_tgt /home/fdamain/mol-edit/data/qed/tgt_valid.csv \
+									  -save_data /home/fdamani/mol-edit/data/qed/
