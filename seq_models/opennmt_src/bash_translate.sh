d4a7f081024b2921d421e39815311a0e0f4e743f
fdamani <farhand7@gmail.com>
2020-03-04 20:24:29 -0500

    .

--- seq_models/opennmt_src/bash_translate.sh
+++
@@ -1,5 +0,0 @@
-python ../../OpenNMT-py/translate.py -model /tigress/fdamani/mol-edit-output/onmt-logp04/checkpoints/model-rnnenc-rnndec-1layer-500wordembed-500embed-dotattention-adamoptim_step_100000.pt \
-					-random_sampling_topk 1 \
-					-beam_size 5 \
-					-src /tigress/fdamani/mol-edit-data/data/logp04/src_baseline_test.csv \
-					-output /tigress/fdamani/mol-edit-output/onmt-logp04/preds/output-baselinetest-5beam-model-rnnenc-rnndec-1layer-500wordembed-500embed-dotattention-adamoptim_step_100000.csv
--- seq_models/opennmt_src/bash_translate_random.sh
+++
@@ -1,8 +0,0 @@
-python ../../OpenNMT-py/translate.py -model /tigress/fdamani/mol-edit-output/onmt-logp04/checkpoints/train_valid_share/model-mlpattention/model-brnnenc-rnndec-2layer-600wordembed-600embed-shareembedding-mlpattention-adamoptim_step_99000.pt \
-					-random_sampling_topk -1 \
-					-random_sampling_temp 1.0 \
-					-beam_size 1 \
-					-seed 829 \
-					-gpu 0 \
-					-src /tigress/fdamani/mol-edit-data/data/logp04/src_baseline_test.csv \
-					-output /tigress/fdamani/mol-edit-output/onmt-logp04/preds/train_valid_share/output-firstiter-baseline-randomsampling-best-model-brnnenc-rnndec-2layer-600wordembed-600embed-shareembedding-mlpattention-adamoptim_step_99000.csv
